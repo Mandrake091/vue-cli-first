06f2413f1b69f81bdb06ebc4941f2c50d9a80de4
Paolo Cannistraro <wasdgamer91@gmail.com>
2022-05-03 16:26:04 +0200

    scaffholding

--- src/App.vue
+++ src/App.vue
@@ -2,21 +2,24 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <MainTitle/>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import MainTitle from './components/MainTitle.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    MainTitle
   }
 }
 </script>
 
-<style>
+<style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
---
+++ src/components/MainTitle.vue
@@ -0,0 +1,10 @@
+<template>
+<h1>Hello world con VueCLi</h1>
+</template>
+<script>
+export default {
+    name: 'MainTitle'
+}
+</script>
+<style scoped>
+</style>
